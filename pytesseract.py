ae8919d3a96fcb083f3801bf1bd69b8fbd17bdf7
Jaeyeong Hwang <99489807+jwyeeh-dev@users.noreply.github.com>
2023-07-12 10:13:32 +0900

    add pytesseract model

---
+++ pytesseract.py
@@ -0,0 +1,11 @@
+import cv2
+import pytesseract
+from PIL import Image
+
+from PIL import Image
+
+import pytesseract
+
+# Simple image to string
+print(pytesseract.image_to_string(Image.open('/Users/jwyeeh/Desktop/sc.png')))
+
